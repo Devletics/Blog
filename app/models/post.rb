c5c9c6758ac4ce1272d567285e5e7bd236c89115
jf-blyncsy <joshfluke@Joshs-MacBook-Pro.local>
2016-06-02 12:32:39 -0600

    Blog
    Push blog to github

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  has_many :comments, dependent: :destroy ##this is referencing the posting controller telling it they are related.
+  validates :title, presence: true, length: {minimum: 5}
+  validates :body, presence: true
 end
